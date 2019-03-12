5710a7473e998e8e48ca25312259414c7d5b90b4
zpei100 <zhengqing.pei@gmail.com>
2019-03-12 14:52:20 -0400

    hero layout includes images with split. Need to change object fit, and make the split static

--- src/client/components/hero/hero.jsx
+++ src/client/components/hero/hero.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Info from '../info/info.jsx';
 import Search from '../search/search.jsx';
+import Image01 from './images/hero01.jpg';
+import Image02 from './images/hero02.jpg';
 
 export default class Hero extends Component {
   render() {
     return (
       <div id="hero" className="d-flex">
-        <div className="middle w-100">
+        <div id="hero-description" className="middle w-100">
           <Info></Info>
           <Search></Search>
         </div>
+        <div className="hero-image hero-left"><img src={Image02}/></div>
+        <div className="hero-image hero-right"><img src={Image01}/></div>
       </div>
     );
   }
--- src/client/components/slide/slide.jsx
+++ src/client/components/slide/slide.jsx
@@ -5,12 +5,34 @@ import Cover from './cover.jsx';
 
 export default class Slide extends Component {
   componentDidMount() {
-    const container = this.container;
-    const pContainer = container.position().top;
-    const cover = this.container.find('.slide-cover');
-    const wait = 20;
+    const $container = this.container;
+    const $cover = $container.find('.slide-cover');
+    const $window = $(window);
+
+    const pContainer = $container.position().top;
+    
+    const wait = 15;
     let right = '0%';
     let time = new Date();
+
+    const startView = 0.6;
+    const endView = 0;
+
+    $window.scroll(function() {
+      if(new Date() - time > wait) {
+        time = new Date();
+        const pWindow = $window.scrollTop();
+        const pDiff = pContainer - pWindow;
+        const pPercentage = pDiff / $container.height();
+
+        if(pPercentage > startView) $cover.css({right: '0%'});
+        else if(pPercentage < endView) $cover.css({right: '-60%'});
+        else {
+          const percentageShown = -0.6/(endView - startView) * (pPercentage - startView) * 100;
+          $cover.css({right: `${percentageShown}%`})
+        }
+      }
+    })
 
     // $(window).scroll(function() {
     //   if(new Date() - time > wait) {
@@ -35,7 +57,7 @@ export default class Slide extends Component {
       <div ref={e => this.container = $(e)} className="slide">
         <Description {...this.props}/>
         <ImageSlide />
-        {/* <Cover /> */}
+        <Cover />
       </div>
     );
   }
