3a078f861a9c080a087aadd54679048b5f2a68a2
zpei100 <zhengqing.pei@gmail.com>
2019-03-13 21:14:24 -0400

    search bar polished

--- src/client/components/search/search.jsx
+++ src/client/components/search/search.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Flag from './flag.jsx';
+import $ from 'jquery';
 
 export default class Search extends Component {
+  highlight() {
+    this.search.css({border: '2.5px #80CED7 solid'});
+  }
+
+  removeHighlight() {
+    this.search.css({border: '2.5px solid transparent'})
+  }
+
   render() {
     return (
-      <div className="search w-auto o-hidden">
-        <div className="h-100">
+      <div ref={e => this.search = $(e)} className="search w-auto o-hidden" style={{display: 'table'}}>
+        <div className="h-100" style={{display: 'table-cell'}}>
           <span className="p-2"><Flag/></span>
-          <input 
+          <input
+            onFocus={e => this.highlight()} 
+            onBlur={e => this.removeHighlight()}
             type="text" 
             className="px-14 ls-1 border-transparent"
             placeholder="Enter your delivery address"
@@ -15,6 +26,7 @@ export default class Search extends Component {
         </div>
         <button 
           className="px-14 ls-1 border-transparent heavy"
+          style={{display: 'table-cell'}}
         >
           Find Restaurants
         </button>
